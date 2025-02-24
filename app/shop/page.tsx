614f6843a9d36b8a4f819ab4436055771bd34963
Valentine Omonya <valomosh254@gmail.com>
2025-02-24 22:08:13 +0300

    Refactor shop page layout and improve UI

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,10 +1,10 @@
+import React from 'react'
 import Filters from '@/screens/shop/widgets/Filters'
 import Products from '@/screens/shop/widgets/Products'
-import React from 'react'
 
 const Shop = () => {
   return (
-    <section className='flex mt-12 px-2'>
+    <section className='flex mt-12  px-2'>
         <Filters/>
         <Products/>
     </section>
