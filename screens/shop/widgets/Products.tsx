e94ecafc485efcc39f05d04fd20f5a52d113aea5
Valentine Omonya <valomosh254@gmail.com>
2025-02-24 22:09:25 +0300

    Add layout toggle functionality to Products component

--- screens/shop/widgets/Products.tsx
+++ screens/shop/widgets/Products.tsx
@@ -1,13 +1,30 @@
+"use client"
 import ProductCard from "@/components/common/card/ProductCard";
 import SortingControls from "../components/SortingControls";
 import PaginationControls from "../components/PaginationControls";
 import LayoutControls from "../components/LayoutControls";
 import ProductCardList from "@/components/common/card/ProductCardList";
+import { useLayout } from "@/hooks/useLayout";
+import { motion, AnimatePresence } from "framer-motion";
+import { useShopFilter } from "@/hooks/useFilter";
 
 const Products = () => {
+  const { layout } = useLayout();
+  const {
+    isMobileFilterOpen,
+    toggleMobileFilter,
+  } = useShopFilter();
+  // Animation variants
+  const cardVariants = {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 0, y: -20 }
+  };
+
   return (
     <div className="w-full flex-1">
       <div className="flex items-center justify-between mb-4">
+<button  onClick={toggleMobileFilter}>open</button>
         <SortingControls />
         <div className="flex items-center gap-x-2">
           <PaginationControls />
@@ -15,12 +32,37 @@ const Products = () => {
         </div>
       </div>
 
-      <div className="grid gap-2" >{/*  gap-2"*/}
-        {[...Array(20)].map((_, index) => (
-        //   <ProductCard key={index} />
-        <ProductCardList key={index}/>
-        ))}
-      </div>
+      <motion.div
+        key={layout}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={{ duration: 0.2 }}
+        className={`grid gap-4 ${
+          layout === "grid"
+            ? "grid-cols-2 lg:grid-cols-4"
+            : " grid-cols-2 xs:grid-cols-1"
+        }`}
+      >
+        <AnimatePresence mode="wait">
+          {[...Array(20)].map((_, index) => (
+            <motion.div
+              key={index}
+              variants={cardVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
+              transition={{ duration: 0.3 }}
+            >
+              {layout === "grid" ? (
+                <ProductCard />
+              ) : (
+                <ProductCardList />
+              )}
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </motion.div>
     </div>
   );
 };
