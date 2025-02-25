715b8413e29be9ff39471252af6cd90b86178921
Valentine Omonya <valomosh254@gmail.com>
2025-02-25 21:21:13 +0300

    feat: update shop page layout and logic

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Filters from '@/screens/shop/widgets/Filters'
 import Products from '@/screens/shop/widgets/Products'
+import MobileFilter from '@/screens/shop/widgets/MobileFilter'
 
 const Shop = () => {
   return (
     <section className='flex mt-12  px-2'>
         <Filters/>
         <Products/>
+        <MobileFilter/>
     </section>
   )
 }
