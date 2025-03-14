6e490f9b6803daaf321ff6c47fec8f2a7bcc9758
Valentine Omonya <valomosh254@gmail.com>
2025-03-14 05:57:29 +0300

    Modify VendorHero component

--- screens/vendor/components/VendorHero.tsx
+++ screens/vendor/components/VendorHero.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import { BiLocationPlus, BiLogoFacebook } from "react-icons/bi";
+import Image from "next/image"; import { BiLocationPlus, BiLogoFacebook } from "react-icons/bi";
 import { BsHandbag, BsPhone, BsStarFill } from "react-icons/bs";
 import { LiaGoogle } from "react-icons/lia";
 import {
@@ -37,9 +36,18 @@ const VendorHero = () => {
     { icon: <FaPinterestP size={16} />, name: "pinterest" },
   ];
   return (
-    <div className="w-full aspect-[20/9] rounded-md overflow-hidden grid grid-cols-12 relative">
-      <div className=" col-span-12 lg:col-span-4 bg-black/80 text-white h-full relative z-10 e"></div>p-10">
-        <div className="w-16 aspect-square rounded-full overflow-hidden">
+    <div className="w-full xl:aspect-[20/9] rounded-md overflow-hidden grid grid-cols-12 relative border border-gray-300">
+      <div className="relative flex xl:hidden w-full h-96 col-span-12">
+        <Image
+          fill
+          priority
+          className="object-cover absolute w-full h-full "
+          src="/store-banner.png"
+          alt="Store Banner"
+      />
+      </div>
+      <div className=" col-span-12 xl:col-span-4 xl:bg-black/80  xl:text-white h-full relative z-10 p-10 pb-4 xl:pb-10 flex items-center justify-center flex-col">
+        <div className="w-20 xl:w-16 aspect-square rounded-full overflow-hidden xl:static absolute -top-10">
           <Image
             src="/avatar.png"
             alt="Vendor Profile"
@@ -48,22 +56,22 @@ const VendorHero = () => {
           />
         </div>
         <h2 className="text-lg font-semibold my-4 ">Vendor 1</h2>
-        <ul className="text-gray-200 space-y-5">
+        <ul className="text-gray-600 xl:text-gray-200 space-y-2 xl:space-y-5  flex flex-col items-center xl:block">
           {vendorInfo.map((item, index) => (
-            <li key={index} className="flex items-center gap-x-2">
-              {item.icon}
+            <li key={index} className="flex items-center gap-x-2 xl:text-start text-center">
+              <span className="hidden xl:inline-block">{item.icon}</span>
               <span className="text-sm">{item.text}</span>
             </li>
           ))}
         </ul>
 
-        <ul className="flex items-center gap-x-4 mt-12           ">
+        <ul className="flex items-center gap-x-4 mt-6 xl:mt-12           ">
           {socialIcons.map((social) => (
             <li
               key={social.name}
               className="hover:text-primary transition-colors"
             >
-              <button className="text-xl rounded-full  p-2 border border-white">
+              <button className="text-xl rounded-full  p-2 border border-gray-400 xl:border-white">
                 {social.icon}
               </button>
             </li>
@@ -73,12 +81,11 @@ const VendorHero = () => {
       <Image
         fill
         priority
-        className="object-cover absolute w-full h-full"
         src="/store-banner.png"
+        className="object-cover absolute w-full h-full hidden xl:inline-block "
         alt="Store Banner"
       />
     </div>
   );
 };
-
 export default VendorHero;
