c0715b415284b193cbf43d42de4b4ed53b050e30
Valentine Omonya <valomosh254@gmail.com>
2025-03-14 05:57:33 +0300

    Modify VendorProductSearch component

--- screens/vendor/components/VendorProductSearch.tsx
+++ screens/vendor/components/VendorProductSearch.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const VendorProductSearch = () => {
   return (
-    <div>VendorProductSearch</div>
-  )
-}
+    <div className="flex items-center w-full sm:w-fit">
+      <input className="py-2 px-4 outline-none border w-full" />
 
-export default VendorProductSearch
+      <button className="bg-[#ff9933] text-white px-6 py-2 rounded-sm">
+        Search
+      </button>
+    </div>
+  );
+};
+
+export default VendorProductSearch;
