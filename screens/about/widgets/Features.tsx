39e2ea19ac3018b238a95fdb1042dd56395b2431
Valentine Omonya <valomosh254@gmail.com>
2025-03-16 19:42:30 +0300

    Added features section

---
+++ screens/about/widgets/Features.tsx
@@ -0,0 +1,45 @@
+import { Accordion } from "../compoents/Accordion";
+import Image from "next/image";
+
+const Services = () => {
+  return (
+    <section className="px-4 grid grid-cols-2 h-[520px] justify-center items-center gap-x-8 my-6">
+      <div className="h-full w-full ">
+        <h1 className="text-2xl md:text-3xl font-bold  mb-12">
+          We Provide Continuous & Kind Service for Customers
+        </h1>
+
+        <Accordion
+          items={[
+            {
+              title: "Sales Management",
+              content:
+                "Lorem ipsum dolor sit eiusamet, consectetur adipiscing elit...",
+            },
+            {
+              title: "Sales Management",
+              content:
+                "Lorem ipsum dolor sit eiusamet, consectetur adipiscing elit...",
+            },
+            {
+              title: "Sales Management",
+              content:
+                "Lorem ipsum dolor sit eiusamet, consectetur adipiscing elit...",
+            },
+          ]}
+        />
+      </div>
+      <div className="relative w-full h-full rounded-md overflow-hidden ">
+        <Image
+          src="/about.png"
+          className="absolute object-cover w-full h-full"
+          alt="About image"
+          fill
+          quality={100}
+          priority
+        />
+      </div>
+    </section>
+  );
+};
+export default Services;
