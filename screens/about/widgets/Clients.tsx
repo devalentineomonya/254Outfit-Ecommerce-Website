747fefbed998e3ae32948f7768ab05a7ef85d586
Valentine Omonya <valomosh254@gmail.com>
2025-03-16 23:55:38 +0300

    Modified Clients.tsx

--- screens/about/widgets/Clients.tsx
+++ screens/about/widgets/Clients.tsx
@@ -1,8 +1,8 @@
 const Clients = () => {
   return (
     <section className="container mx-auto px-4 py-16">
-      <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 max-w-2xl mx-auto">
-        We Boost Our Clients’ Bottom Line by Optimizing Their Growth Potential
+      <h1 className="text-3xl md:text-4xl font-bold text-center mb-8 ">
+        We Boost Our Clients&pos; Bottom Line by Optimizing Their Growth Potential
       </h1>
 
       <div className="max-w-3xl mx-auto">
