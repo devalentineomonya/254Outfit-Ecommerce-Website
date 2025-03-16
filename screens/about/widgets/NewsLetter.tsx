4e14bf78279856e50d05e477725a8f4b77549015
Valentine Omonya <valomosh254@gmail.com>
2025-03-16 23:55:41 +0300

    Modified NewsLetter.tsx

--- screens/about/widgets/NewsLetter.tsx
+++ screens/about/widgets/NewsLetter.tsx
@@ -1,7 +1,9 @@
+import { BsArrowRight } from "react-icons/bs";
+
 const Newsletter = () => {
   return (
-    <section className="container mx-auto px-4 py-16">
-      <div className="max-w-3xl mx-auto text-center">
+    <section className="px-4 py-16">
+      <div className=" text-center">
         <h2 className="text-3xl md:text-4xl font-bold mb-4">
           SUBSCRIBE TO OUR NEWSLETTER
         </h2>
@@ -16,8 +18,9 @@ const Newsletter = () => {
             placeholder="Your E-mail Address"
             className="flex-1 min-w-[250px] px-6 py-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
           />
-          <button className="px-8 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-semibold">
-            SUBSCRIBE →
+          <button className="flex items-center gap-x-3 px-8 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors font-semibold">
+            <span>SUBSCRIBE</span>
+            <BsArrowRight />
           </button>
         </div>
       </div>
