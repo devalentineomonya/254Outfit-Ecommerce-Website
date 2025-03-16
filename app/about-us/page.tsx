ff73730a84420cb6353ba54e4033f0721f7c2e03
Valentine Omonya <valomosh254@gmail.com>
2025-03-16 19:41:15 +0300

    Modified About Us page

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import Awards from "@/screens/about/widgets/Awards";
+import Clients from "@/screens/about/widgets/Clients";
+import Services from "@/screens/about/widgets/Features";
+import Newsletter from "@/screens/about/widgets/NewsLetter";
+import Stats from "@/screens/about/widgets/Stats";
 
 const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <section className="flex flex-col mt-12  px-2">
+      <Services />
+      <Stats />
+      <Clients />
+      <Awards />
+      <Newsletter />
+    </section>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
